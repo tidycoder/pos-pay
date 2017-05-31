27a03294f1446fb6f5b62d6a28798ced47238c12
tidycoder <tidycoder@gmail.com>
2017-05-31 14:41:39 +0800

    misc

--- pos.js
+++ pos.js
@@ -1,5 +1,8 @@
 
 var SerialPortFactory = require('chrome-serialport');
+SerialPortFactory.extensionId = 'blpiikpknmobbfcmohnbmkjcobihpgaa';
+
+
 var SerialPort = SerialPortFactory.SerialPort;
 
 var decodePosData = require('./pos_recv');
@@ -7,7 +10,25 @@ var decodePosData = require('./pos_recv');
 var encodePosData = require('./pos_send');
 
 
+var payElement = document.getElementById("test");
 
 
+var exists = 'COM5';
+var exists2 = 'COM6';
+var port2 = new SerialPort(exists2, {}, function () {
+ 	port2.once('data', function(recvData) {
+ 	alert("success receive data!!");
+ 		console.log(recvData);
+  	});
+});
 
+payElement.onclick = function() {
+
+	var port = new SerialPort(exists, function() {
+        port.write(encodePosData(), function(err) {
+         	port.close();
+        })
+    })
+
+}
 
